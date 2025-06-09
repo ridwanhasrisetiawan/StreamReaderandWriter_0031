01df3e7570e3dc8701460453ab8f1d41bf9a4f35
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-06-09 10:35:49 +0700

    implementasi exception handling dengan throw

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -3,5 +3,9 @@ using namespace std;
 
 int main()
 {
-    
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5;
+        cout << "Pertanyaan tidak akan dieksekusi" << endl;
+    }
 }
