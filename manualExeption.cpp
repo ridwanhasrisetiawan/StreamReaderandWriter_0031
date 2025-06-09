f058115c73d7e14e76b64483a01a94ba8f5e5843
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2025-06-09 10:40:10 +0700

    menangani pengecualian dengan catch

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -8,4 +8,14 @@ int main()
         throw 0.5;
         cout << "Pertanyaan tidak akan dieksekusi" << endl;
     }
+    catch (int a) {
+
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...) {
+
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
 }
